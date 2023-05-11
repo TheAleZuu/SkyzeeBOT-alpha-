76befdf47f0bccb16a0aecd10476f683ac289432
TheAleZuu <alexmine9999@gmail.com>
2023-05-11 01:09:59 -0300

    descarga de sesión fixed. Arreglar asincronización de código

--- index.js
+++ index.js
@@ -7,69 +7,74 @@ const Dropbox = require('dropbox').Dropbox;
 
 const prefix = /^[!/.]/;
 
-var client = new function () {
-    this.dropbox = new Dropbox({
-        accessToken: "sl.BeGpwuw7rx54ePEEQ8nt-PB_AZenoMcqEha-WnNsnngXJL3-F4KkKF0gL__QBLLZATGxwD9LJAklnws3EvEU0kJ8vG9R4M_Gv29BP8Ziku0BlKrK6_spJtbr-JuJYIlk_O3b_Fw"
-    });
-
-    this.dropbox.filesDownloadZip({
-        path: '/session/'
-    }).then(response => {
-        console.log(response);
-        // Crear un objeto JSZip a partir de los datos del archivo ZIP
-        return JSZip.loadAsync(response.result.fileBinary);
-    }).then(zip => {
-        // Descomprimir el archivo ZIP
-        return zip.forEach((relativePath, file) => {
-            // verificar si el archivo es una carpeta
-            if (!file.dir) {
-                var dir = relativePath.replace(/\/$/, '');
-                fs.mkdir(`./${dir}`, (err) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        console.log(`[!] Descargando sesión de WhatsApp (FOLDER): ${relativePath}/...`);
-                    }
+async () => {
+    const client = await new function () {
+        let SESSION_FILE_PATH = '/session/';
+        
+        async () => {
+            this.dropbox = new Dropbox({
+                accessToken: "sl.BeL__yWV3JcdCPP2HD8XXT42mBI6UEn8NqQS9fmD4qUpVBB6SFEogHWH7Dam3ZDxSGNRsWeFO0BbPME9X-pNTUXZO_4VyQbQdPCaxRdon4_oNewcWi5kF-B6MVi1TlzkpITAGOY"
+            });
+        
+            if (!fs.existsSync(SESSION_FILE_PATH)) {
+                console.log('[!] No se encontró el archivo de sesión, descargando...');
+                await this.dropbox.filesDownloadZip({
+                    path: '/session/'
+                }).then(response => {
+                    console.log(response);
+                    // Crear un objeto JSZip a partir de los datos del archivo ZIP
+                    return JSZip.loadAsync(response.result.fileBinary);
+                }).then(zip => {
+                    // Descomprimir el archivo ZIP
+                    return zip.forEach((relativePath, file) => {
+                        // verificar si el archivo es una carpeta
+                        if (file.dir) {
+                            console.log(`[!] Descargando sesión de WhatsApp...  ${relativePath} (FOLDER)`);
+                            let dir = relativePath.replace(/\/$/, '');
+                            fs.mkdirSync(dir, (err) => console.error(err));
+                        } else {
+                            // Si es un archivo, extraer el contenido
+                            console.log(`[!] Descargando sesión de WhatsApp...  ${relativePath} (FILE)`);
+                            file.async('string').then(content => {
+                                let filename = fs.openSync(relativePath, 'w');
+                                fs.writeSync(filename, content);
+                                fs.closeSync(filename);
+                            });
+                        };
+                    });
                 });
             } else {
-                // Si es un archivo, extraer el contenido
-                file.async('string').then(content => {
-                    var filename = relativePath.split('/').pop();
-                    fs.write(`./${filename}`, content, (err) => {
-                        console.error(err)
-                    })
-                    console.log(`[!] Descargando sesión de WhatsApp (FILE): ${relativePath}/${file}...`);
-                });
-            }
-        });
+                console.log('[!] Archivo de sesión encontrada!');
+            };
+            
+            this.wweb_js = new Client({
+                authStrategy: new LocalAuth({
+                    clientId: 'SKYZEEBOT',
+                    dataPath: SESSION_FILE_PATH
+                })
+            });
+        };
+    };
+
+    const SkyzeeBOT = client.wweb_js;
+
+    SkyzeeBOT.initialize();
+
+    SkyzeeBOT.on('qr', (qr) => {
+        qrcode.generate(qr, {small: true});
     });
 
-    this.wweb_js = new Client({
-        authStrategy: new LocalAuth({
-            clientId: 'SKYZEEBOT',
-            dataPath: '/session/'
+    SkyzeeBOT.on('authenticated', () => {
+        console.log('SkyzeeBOT activo');
+    });
+
+    SkyzeeBOT.on('ready', () => {
+        SkyzeeBOT.on('message', (wweb_allResult) => {
+            const msgConfig = wweb_allResult.Message._data;
+            const isCommand = prefix.test(msgContent);
+            const command = isCommand ? msg.slice(1) : null;
+            console.log(msg);
+            if (!isCommand) return;
         })
     });
 };
-
-const SkyzeeBOT = client.wweb_js;
-
-SkyzeeBOT.initialize();
-
-SkyzeeBOT.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-});
-
-SkyzeeBOT.on('authenticated', () => {
-    console.log('SkyzeeBOT activo');
-});
-
-SkyzeeBOT.on('ready', () => {
-    SkyzeeBOT.on('message', (wweb_allResult) => {
-        const msgConfig = wweb_allResult.Message._data;
-        const isCommand = prefix.test(msgContent);
-        const command = isCommand ? msg.slice(1) : null;
-        console.log(msg);
-        if (!isCommand) return;
-    })
-});
